ce043cb47ed7e53b5cfaf2af85671236c25d33a2
Jake <lowbak1998@hotmail.com>
2019-08-05 20:45:17 +0800

    first commit

---
+++ src/Job.java
@@ -0,0 +1,19 @@
+
+public class Job implements Comparable <Job>{
+		int profit;
+		int deadline;
+		String jobid;
+		
+		public Job (String jobid , int deadline , int profit)
+		{
+			this.profit= profit;
+			this.deadline = deadline;
+			this.jobid = jobid;
+		}
+
+		@Override
+		public int compareTo(Job otherJob) {
+			return otherJob.profit- this.profit;
+		}
+	}
+	
